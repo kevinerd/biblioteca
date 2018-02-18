6708bab5b0ef15e90b75f210902133ed8775f3e7
Kevin Firmani <kevinjf2011@gmail.com>
2018-02-18 20:05:13 -0300

    Vistas de login y contacto

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -9,4 +9,8 @@ class SiteController extends Controller
     public function home(){
         return view('site.index');
     }
+
+    public function contacto(){
+        return view('site.contacto');
+    }
 }
